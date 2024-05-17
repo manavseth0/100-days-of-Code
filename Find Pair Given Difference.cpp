b85ac924293c3243f1003beffa308e848a10fcc1
manavseth0 <145740764+manavseth0@users.noreply.github.com>
2024-05-17 11:56:42 +0530

    Update Find Pair Given Difference.cpp

--- Find Pair Given Difference.cpp
+++ Find Pair Given Difference.cpp
@@ -3,21 +3,16 @@ class Solution {
     int findPair(int n, int x, vector<int> &arr) {
         // code here
         sort(arr.begin(), arr.end());
-        int i=0, j=1;
-        while(j<n){
-            int diff=abs(arr[j]-arr[i]);
-
-            if(diff==x){
+        int i = 0, j = 1;
+        
+        while(i<n && j<n){
+            if(i!=j && abs(arr[j]-arr[i]) == x)
                 return 1;
-            }
-            else if(diff<x){
-                j++;
-            }
-            else{
+            else if(abs(arr[j]-arr[i]) > x)
                 i++;
-            }
+            else
+                j++;
         }
-
         return -1;
     }
 };
