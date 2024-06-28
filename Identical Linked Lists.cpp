001db729b14841d94c36c5cad53100eebe030878
manavseth0 <145740764+manavseth0@users.noreply.github.com>
2024-06-29 01:04:58 +0530

    Create Identical Linked Lists.cpp

---
+++ Identical Linked Lists.cpp
@@ -0,0 +1,20 @@
+bool areIdentical(struct Node *head1, struct Node *head2) {
+    // Code here
+    Node* h1=head1;Node* h2=head2;
+
+    while (h1!=NULL && h2!=NULL){
+
+        if(h1->data!=h2->data) return false;
+
+        h1=h1->next;
+
+        h2=h2->next;
+
+    }
+
+    if(h1!=NULL || h2!=NULL) return false;
+
+    return true;
+
+
+}
