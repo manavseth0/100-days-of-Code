d0af51ade91df5faeee8ed93020dacccbf7c37ea
manavseth0 <145740764+manavseth0@users.noreply.github.com>
2024-08-09 23:05:56 +0530

    Create Maximize Array Value After Rearrangement.cpp

---
+++ Maximize Array Value After Rearrangement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  
+  const int MOD = 1e9 + 7;
+  
+  public:
+    int Maximize(vector<int> &arr) {
+        // Complete the function
+        long long int sum=0;
+        sort(arr.begin(),arr.end());
+        for(long long int i=0;i<arr.size();i++){
+            sum+=i*arr[i];
+        }
+        return sum%MOD;
+    }
+};
